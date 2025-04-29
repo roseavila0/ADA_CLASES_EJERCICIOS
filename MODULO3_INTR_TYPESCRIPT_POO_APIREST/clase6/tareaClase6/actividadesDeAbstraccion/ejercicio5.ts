1bb9049b3a119219824ec75b12d8f7ef876915ab
roseavila0 <rosa.avilacaliz@gmail.com>
2025-04-29 00:52:48 -0500

    subiendo carpeta clase 6, tarea ejercicios completos

--- MODULO3_INTR_TYPESCRIPT_POO_APIREST/clase6/tareaClase6/actividadesDeAbstraccion/ejercicio5.ts
+++ MODULO3_INTR_TYPESCRIPT_POO_APIREST/clase6/tareaClase6/actividadesDeAbstraccion/ejercicio5.ts
@@ -5,3 +5,37 @@ Diseña un sistema de transporte público. Define:
 • Una clase abstracta Transporte con el método abstracto mover().
 Implementa ambas estructuras en una clase concreta AutobusElectrico.
 */
+
+interface VehiculoElectrico {
+
+    cargarBateria():void;
+}
+
+
+// Clase abstracta con método abstracto
+abstract class Transporte{
+    abstract mover():void; 
+}
+
+
+
+// Clase concreta que extiende la abstracta y también implementa la interfaz
+class AutobusElectrico  extends Transporte implements VehiculoElectrico{
+
+    cargarBateria(): void {
+        console.log(`Cargando batería del autobus eléctrico`);
+    }
+
+    mover():void{
+        console.log(`El autobús eléctrico se esta moviendo`);
+    }
+}
+
+const busElectrico =  new AutobusElectrico();
+busElectrico.cargarBateria();
+busElectrico.mover();
+
+
+
+
+
--- MODULO3_INTR_TYPESCRIPT_POO_APIREST/clase6/tareaClase6/actividadesDeAbstraccion/ejercicio6.ts
+++ MODULO3_INTR_TYPESCRIPT_POO_APIREST/clase6/tareaClase6/actividadesDeAbstraccion/ejercicio6.ts
@@ -10,3 +10,45 @@ Implementa dos subclases: Ropa y Electronica.
 Añade un método concreto en la clase abstracta para mostrar la información del
 producto.
 */
+
+abstract class Producto {
+    constructor(protected nombre:string, protected precio:number) {};
+
+    abstract calcularDescuento():number;
+
+    infoProducto():void{
+        console.log(`El producto ${this.nombre} cuesta $${this.precio.toFixed(2)}.`);
+    }
+}
+
+//La subclase Ropa aplicará un 10% de descuento.
+class Ropa extends Producto {
+    
+    calcularDescuento(): number{
+        const precioConDescuento = this.precio  - (this.precio * 10 / 100 )
+        console.log(`Este producto tiene un 10% de Dscto, su precio final es $${precioConDescuento.toFixed(2)}`)
+        return precioConDescuento;
+    }
+    
+}
+
+
+// La subclase Electronica tendrá un 5% de descuento.
+class Electronica extends Producto{
+
+    calcularDescuento(): number{
+        const precioConDescuento = this.precio  - (this.precio * 5 / 100 )
+        console.log(`Este producto tiene un 5% de Dscto, su precio final es $${precioConDescuento.toFixed(2)}`)
+        return precioConDescuento;
+    }
+}
+
+const blusa = new Ropa ("Blusa blanca, lino.", 15)
+blusa.infoProducto();
+blusa.calcularDescuento();
+
+console.log("-------------------------------------------------------------------------------------")
+const DiscoDuro = new Electronica ("Disco duro 1TB", 92)
+DiscoDuro.infoProducto();
+DiscoDuro.calcularDescuento();
+
